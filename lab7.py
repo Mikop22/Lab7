216eb3cc7ec34084e634d09610ccdc48cdfa8c63
Mikhai Wilson <mikhaischolar@gmail.com>
2025-02-21 19:01:13 -0500

    Add files via upload

---
+++ lab7.py
@@ -0,0 +1,67 @@
+# ECOR 1041 - Bonus
+
+__author__ = ""
+__student_number__ = ""
+
+# ======================================================
+# Exercise 1
+
+# Type your function definition for Exercise 1 here.
+
+# ======================================================
+# Exercise 2
+
+# Type your function definition for Exercise 2 here.
+
+# ======================================================
+# Exercise 3
+
+# Type your function definition for Exercise 3 here.
+
+# ======================================================
+# Exercise 4
+
+# Type your function definition for Exercise 4 here.
+
+# ======================================================
+# Exercise 5
+
+# Type your function definition for Exercise 5 here.
+
+# ======================================================
+# Exercise 6
+
+# Type your function definition for Exercise 6 here.
+
+# ======================================================
+# Exercise 7
+
+# Type your function definition for Exercise 7 here.
+
+# ======================================================
+# Exercise 8
+
+# Type your function definition for Exercise 8 here.
+
+# ======================================================
+# Exercise 9
+
+# Type your function definition for Exercise 9 here.
+
+
+# ======================================================
+# Exercise 10
+
+# Type your function definition for Exercise 10 here.
+
+# ======================================================
+# Exercise 11
+
+# Type your function definition for Exercise 11 here.
+
+# ======================================================
+# Exercise 12
+
+# Type your function definition for Exercise 12 here.
+
+
